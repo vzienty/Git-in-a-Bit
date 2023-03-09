8a3061b2aab4046698e181171761dbac216e4b73
Valerija Zienty <vzienty@worcesterk12.org>
2023-03-09 09:08:44 -0500

    Initial Commit

---
+++ Git in a Bit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Git in a Bit
+//
+//  Created by Valerija Zienty on 3/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git in a Bit/Git_in_a_BitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_in_a_BitApp.swift
+//  Git in a Bit
+//
+//  Created by Valerija Zienty on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Git_in_a_BitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
